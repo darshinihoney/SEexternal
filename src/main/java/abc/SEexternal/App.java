4be29aaa281bf5486105aa1ee53ab516217967dd
angelena <angelena.shiny@gmail.com>
2025-01-16 11:07:41 +0530

    added

---
+++ src/main/java/abc/SEexternal/App.java
@@ -0,0 +1,13 @@
+package abc.SEexternal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welome to se external!" );
+    }
+}
